378f90be11e42d7d863301e0d8fa1917630c5cac
ryfazrin <riansera515@gmail.com>
2023-10-17 15:28:29 +0700

    move: antd default component to new route

---
+++ pages/antd-default.vue
@@ -0,0 +1,216 @@
+<script lang="ts" setup>
+import { message } from 'ant-design-vue'
+
+const checked = shallowRef(true)
+const handleClick = () => {
+  message.info('test')
+}
+
+const columns = [
+  {
+    name: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+  },
+]
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+]
+const placements = ['topLeft', 'top', 'topRight', 'bottomLeft', 'bottom', 'bottomRight'] as const
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+}
+
+const validateMessages = {
+  // eslint-disable-next-line no-template-curly-in-string
+  required: '${label} is required!',
+  types: {
+    // eslint-disable-next-line no-template-curly-in-string
+    email: '${label} is not a valid email!',
+    // eslint-disable-next-line no-template-curly-in-string
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    // eslint-disable-next-line no-template-curly-in-string
+    range: '${label} must be between ${min} and ${max}',
+  },
+}
+
+const formState = reactive({
+  user: {
+    name: '',
+    age: undefined,
+    email: '',
+    website: '',
+    introduction: '',
+  },
+})
+const onFinish = (values: any) => {
+  // eslint-disable-next-line no-console
+  console.log('Success:', values)
+}
+
+const open = ref<boolean>(false)
+
+const showModal = () => {
+  open.value = true
+}
+
+const handleOk = (e: MouseEvent) => {
+  open.value = false
+}
+</script>
+
+<template>
+  <div>
+    <a-button @click="handleClick">
+      按钮
+    </a-button>
+    <a-switch v-model:checked="checked" />
+
+    <a-table :columns="columns" :data-source="data">
+      <template #headerCell="{ column }">
+        <template v-if="column.key === 'name'">
+          <span>
+            <smile-outlined />
+            Name
+          </span>
+        </template>
+      </template>
+
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'name'">
+          <a>
+            {{ record.name }}
+          </a>
+        </template>
+        <template v-else-if="column.key === 'tags'">
+          <span>
+            <a-tag
+              v-for="tag in record.tags"
+              :key="tag"
+              :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+            >
+              {{ tag.toUpperCase() }}
+            </a-tag>
+          </span>
+        </template>
+        <template v-else-if="column.key === 'action'">
+          <span>
+            <a>Invite 一 {{ record.name }}</a>
+            <a-divider type="vertical" />
+            <a>Delete</a>
+            <a-divider type="vertical" />
+            <a class="ant-dropdown-link">
+              More actions
+              <down-outlined />
+            </a>
+          </span>
+        </template>
+      </template>
+    </a-table>
+
+    <a-space style="display: flex; flex-wrap: wrap">
+      <template v-for="placement in placements" :key="placement">
+        <a-dropdown :placement="placement" arrow>
+          <a-button>{{ placement }}</a-button>
+          <template #overlay>
+            <a-menu>
+              <a-menu-item>
+                <a href="javascript:;">1st menu item</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a href="javascript:;">2nd menu item</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a href="javascript:;">3rd menu item</a>
+              </a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+      </template>
+    </a-space>
+
+    <a-form
+      :model="formState"
+      v-bind="layout"
+      name="nest-messages"
+      :validate-messages="validateMessages"
+      @finish="onFinish"
+    >
+      <a-form-item :name="['user', 'name']" label="Name" :rules="[{ required: true }]">
+        <a-input v-model:value="formState.user.name" />
+      </a-form-item>
+      <a-form-item :name="['user', 'email']" label="Email" :rules="[{ type: 'email' }]">
+        <a-input v-model:value="formState.user.email" />
+      </a-form-item>
+      <a-form-item :name="['user', 'age']" label="Age" :rules="[{ type: 'number', min: 0, max: 99 }]">
+        <a-input-number v-model:value="formState.user.age" />
+      </a-form-item>
+      <a-form-item :name="['user', 'website']" label="Website">
+        <a-input v-model:value="formState.user.website" />
+      </a-form-item>
+      <a-form-item :name="['user', 'introduction']" label="Introduction">
+        <a-textarea v-model:value="formState.user.introduction" />
+      </a-form-item>
+      <a-form-item :wrapper-col="{ ...layout.wrapperCol, offset: 8 }">
+        <a-button type="primary" html-type="submit">
+          Submit
+        </a-button>
+      </a-form-item>
+    </a-form>
+
+    <div>
+      <a-button type="primary" @click="showModal">
+        Open Modal
+      </a-button>
+      <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </a-modal>
+    </div>
+  </div>
+</template>
