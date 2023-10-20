5a6b87ba4cb59c4fca08bec297a9bfe510926ee7
ryfazrin <riansera515@gmail.com>
2023-10-20 22:42:52 +0700

    added: custom column in table with template & h() render func

--- features/dashboard/ui/index.vue
+++ features/dashboard/ui/index.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
+import type { ColumnsType } from 'ant-design-vue/es/table'
+import { Button, Popconfirm } from 'ant-design-vue'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons-vue'
 import useGetList from '../composables/useGetList'
 
+// Query
 const {
   fetchQuery,
   dataModified,
@@ -9,7 +13,45 @@ const {
 
 const { isLoading, data } = fetchQuery
 
-const columns = [
+// Func
+const confirm = (e: MouseEvent) => {
+  // eslint-disable-next-line no-console
+  console.log(e)
+  message.success('Click on Yes')
+}
+
+const cancel = (e: MouseEvent) => {
+  // eslint-disable-next-line no-console
+  console.log(e)
+  message.error('Click on No')
+}
+
+// Columns
+const columns: ColumnsType<any> = [
+  {
+    title: 'Action',
+    key: 'action',
+    width: '128px',
+    customRender({ record }) {
+      return h(
+        'div',
+        [
+          h(
+            Popconfirm,
+            {
+              title: `Are you sure delete this task ${record.first_name}?`,
+              okText: 'Yes',
+              cancelText: 'No',
+              onConfirm: confirm,
+              onCancel: cancel,
+            },
+            h(Button, h(DeleteOutlined)),
+          ),
+          h(Button, h(EditOutlined)),
+        ],
+      )
+    },
+  },
   {
     title: 'No',
     dataIndex: 'no',
@@ -40,5 +82,24 @@ const columns = [
         handlePageChange(page)
       },
     }"
-  />
+  >
+    <!-- <template #bodyCell="{ column }">
+      <template v-if="column.key === 'action'">
+        <a-popconfirm
+          title="Are you sure delete this task?"
+          ok-text="Yes"
+          cancel-text="No"
+          @confirm="confirm"
+          @cancel="cancel"
+        >
+          <a-button>
+            <DeleteOutlined />
+          </a-button>
+        </a-popconfirm>
+        <a-button>
+          <EditOutlined />
+        </a-button>
+      </template>
+    </template> -->
+  </a-table>
 </template>
