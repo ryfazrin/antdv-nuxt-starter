047bacb06a465ac6e93fdfde4fecf5ef29c2b5e7
ryfazrin <riansera515@gmail.com>
2023-10-24 23:43:20 +0700

    created: useGetList with code useGetList like react logic

--- shared/api/fetch/users.ts
+++ shared/api/fetch/users.ts
@@ -1,23 +1,35 @@
 import { FETCH_USERS } from '~/shared/constants/endpoint'
 import { getData } from '~/utils/http/axiosHelper'
 
-interface Props {
-  key: string[]
-  api: () => Promise<any>
-}
+// interface Props {
+//   key: string[]
+//   api: () => Promise<any>
+// }
 
-function fetchUsers(params: string): Props {
-  const KEY = ['users', params]
+// function fetchUsers(params: string): Props {
+//   console.log('call api')
+//   const KEY = ['USERS', params]
 
-  const API = async () => {
-    const dataUsers = await getData({
-      endpoint: FETCH_USERS(params),
-    })
+//   const API = async () => {
+//     const dataUsers = await getData({
+//       endpoint: FETCH_USERS(params),
+//     })
 
-    return dataUsers?.data
-  }
+//     return dataUsers?.data
+//   }
 
-  return { key: KEY, api: API }
+//   return { key: KEY, api: API }
+// }
+
+async function fetchUsers(page: number) {
+  const dataUsers = await getData({
+    endpoint: FETCH_USERS(paramsToString({
+      page,
+      per_page: 3,
+    })),
+  })
+
+  return dataUsers?.data
 }
 
 export {
