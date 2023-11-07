56512acc2c0b163bd8827e71afabb3c51a50471d
ryfazrin <riansera515@gmail.com>
2023-11-07 15:43:52 +0700

    updated: Menu Sidebar with TODO

--- shared/components/layout/private-layout/MenuSideMenuPrivateLayout.vue
+++ shared/components/layout/private-layout/MenuSideMenuPrivateLayout.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 const selectedKeys1 = ref<string[]>(['dashboard'])
-const openKeys = ref<string[]>(['sub1'])
+const openKeys = ref<string[]>(['sub2'])
 </script>
 
 <template>
@@ -12,28 +12,28 @@ const openKeys = ref<string[]>(['sub1'])
   >
     <a-menu-item key="dashboard">
       <nuxt-link href="/">
-        <user-outlined /> Dashboard
+        <HomeOutlined /> Dashboard
       </nuxt-link>
     </a-menu-item>
     <a-menu-item key="basicQuery">
       <nuxt-link href="/basic-query">
-        <user-outlined /> Basic Query
-      </nuxt-link>
-    </a-menu-item>
-    <a-menu-item key="about">
-      <nuxt-link href="/about">
-        <user-outlined /> About
+        <ThunderboltOutlined /> Basic Query
       </nuxt-link>
     </a-menu-item>
     <a-menu-item key="customAntd">
       <nuxt-link href="/custom-antd">
-        <user-outlined /> Custom Antd
+        <SkinOutlined /> Custom Antd
       </nuxt-link>
     </a-menu-item>
-    <a-sub-menu key="sub3">
+    <a-menu-item key="about">
+      <nuxt-link href="/about">
+        <ProfileOutlined /> About
+      </nuxt-link>
+    </a-menu-item>
+    <a-sub-menu key="sub1">
       <template #title>
         <span>
-          <user-outlined />
+          <AlertOutlined />
           Test
         </span>
       </template>
@@ -43,7 +43,7 @@ const openKeys = ref<string[]>(['sub1'])
         </nuxt-link>
       </a-menu-item>
     </a-sub-menu>
-    <a-sub-menu key="sub1">
+    <!-- <a-sub-menu key="sub2">
       <template #title>
         <span>
           <user-outlined />
@@ -62,48 +62,79 @@ const openKeys = ref<string[]>(['sub1'])
       <a-menu-item key="4">
         option4
       </a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="sub2">
-      <template #title>
-        <span>
-          <laptop-outlined />
-          subnav 2
-        </span>
-      </template>
-      <a-menu-item key="5">
-        option5
-      </a-menu-item>
-      <a-menu-item key="6">
-        option6
-      </a-menu-item>
-      <a-menu-item key="7">
-        option7
-      </a-menu-item>
-      <a-menu-item key="8">
-        option8
-      </a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="sub3">
-      <template #title>
-        <span>
-          <notification-outlined />
-          subnav 3
-        </span>
-      </template>
-      <a-menu-item key="9">
-        option9
-      </a-menu-item>
-      <a-menu-item key="10">
-        option10
-      </a-menu-item>
-      <a-menu-item key="11">
-        option11
-      </a-menu-item>
-      <a-menu-item key="12">
-        option12
-      </a-menu-item>
-    </a-sub-menu>
+    </a-sub-menu> -->
   </a-menu>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ant-menu {
+  padding-bottom: 20px;
+
+  & :deep(li),
+  :deep(li div) {
+    margin: 0 !important;
+    outline: none !important;
+  }
+
+  & :deep(li.ant-menu-item),
+  :deep(.ant-menu-submenu-title),
+  :deep(.ant-menu-item-selected),
+  :deep(.ant-menu-submenu-selected .ant-menu-submenu-title) {
+    height: fit-content;
+    width: calc(100% + 1px);
+
+    & .ant-menu-title-content {
+      margin-inline-start: 16px !important;
+      margin-right: 16px;
+    }
+
+    &:not(.ant-menu-item-only-child) {
+      padding: 8px 16px !important;
+    }
+  }
+
+  & :deep(.ant-menu-submenu.ant-menu-submenu-inline .ant-menu-submenu-title) {
+    min-height: 48px !important;
+  }
+
+  &.ant-menu-root > li.ant-menu-item,
+  :deep(.ant-menu-item) {
+    padding-block: 4px !important;
+  }
+
+  &.ant-menu-inline-collapsed {
+    & :deep(li.ant-menu-item),
+    :deep(.ant-menu-submenu-title),
+    :deep(.ant-menu-item-selected) {
+      padding: 4px 16px !important;
+    }
+
+    & :deep(.ant-menu-item-group-title) {
+      color: #fff;
+      position: relative;
+      height: 10px;
+      margin-top: -5px !important;
+      margin-bottom: 5px !important;
+
+      &:before {
+        content: " ";
+        position: absolute;
+        top: 50%;
+        left: 10px;
+        width: 38px;
+        height: 2px;
+        background: #e2e1e6;
+      }
+    }
+
+    & :deep(li.ant-menu-item-selected),
+    :deep(.ant-menu-submenu-selected > .ant-menu-submenu-title) {
+      &:not(.ant-menu-item-selected.ant-menu-item-only-child) {
+        padding: 20px 16px 10px !important;
+      }
+    }
+  }
+
+  /* TODO: banyak kesalahan di selector dan style */
+}
+</style>
