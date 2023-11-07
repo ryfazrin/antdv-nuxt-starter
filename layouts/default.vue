590d91a52374bd978f53b55b43cf68fa1e408782
ryfazrin <riansera515@gmail.com>
2023-11-07 11:06:20 +0700

    updated: move sidebar & menu to component in private layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,125 +1,55 @@
 <script lang="ts" setup>
 import AntdConfig from '~/shared/styles/AntdConfig.vue'
+import HeaderContentPrivateLayout from '~/shared/components/layout/private-layout/HeaderContentPrivateLayout.vue'
+import FooterContentPrivateLayout from '~/shared/components/layout/private-layout/FooterContentPrivateLayout.vue'
+import SidebarPrivateLayout from '~/shared/components/layout/private-layout/SidebarPrivateLayout.vue'
 
-const selectedKeys1 = ref<string[]>(['dashboard'])
-const openKeys = ref<string[]>(['sub1'])
-const PFDinTextPro = '"PFDinTextPro", sans-serif'
+const collapsed = ref(false)
+
+const setCollapsed = (newCollapsed: boolean) => {
+  collapsed.value = newCollapsed
+}
 </script>
 
 <template>
   <AntdConfig>
     <a-layout has-sider>
-      <a-layout-sider
-        class="text-xs"
+      <SidebarPrivateLayout
+        :collapsed="collapsed"
+        :set-collapsed="setCollapsed"
+      />
+
+      <a-layout
         :style="{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          bottom: 0,
+          background: '#f8f8f8',
+          minHeight: '100vh',
+          marginLeft: collapsed ? '100px' : '260px',
         }"
-        theme="light"
       >
-        <div class="logo" />
-        <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          v-model:openKeys="openKeys"
-          mode="inline"
-          :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-menu-item key="dashboard">
-            <nuxt-link href="/">
-              <user-outlined /> Dashboard
-            </nuxt-link>
-          </a-menu-item>
-          <a-menu-item key="basicQuery">
-            <nuxt-link href="/basic-query">
-              <user-outlined /> Basic Query
-            </nuxt-link>
-          </a-menu-item>
-          <a-menu-item key="about">
-            <nuxt-link href="/about">
-              <user-outlined /> About
-            </nuxt-link>
-          </a-menu-item>
-          <a-menu-item key="customAntd">
-            <nuxt-link href="/custom-antd">
-              <user-outlined /> Custom Antd
-            </nuxt-link>
-          </a-menu-item>
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">
-              option1
-            </a-menu-item>
-            <a-menu-item key="2">
-              option2
-            </a-menu-item>
-            <a-menu-item key="3">
-              option3
-            </a-menu-item>
-            <a-menu-item key="4">
-              option4
-            </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">
-              option5
-            </a-menu-item>
-            <a-menu-item key="6">
-              option6
-            </a-menu-item>
-            <a-menu-item key="7">
-              option7
-            </a-menu-item>
-            <a-menu-item key="8">
-              option8
-            </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">
-              option9
-            </a-menu-item>
-            <a-menu-item key="10">
-              option10
-            </a-menu-item>
-            <a-menu-item key="11">
-              option11
-            </a-menu-item>
-            <a-menu-item key="12">
-              option12
-            </a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout :style="{ marginLeft: '200px' }">
-        <a-layout-header :style="{ background: '#fff', padding: 0 }" />
         <a-layout-content
-          :style="{ margin: '24px 16px 0', overflow: 'initial' }"
+          :style="{
+            padding: 0,
+            margin: 0,
+            display: 'flex',
+            flexDirection: 'column',
+          }"
         >
-          <slot />
+          <!-- HEADER -->
+          <HeaderContentPrivateLayout />
+
+          <!-- CONTENT -->
+          <div
+            style="
+              padding: 20px 30px;
+              min-height: calc(100vh - 132px);
+            "
+          >
+            <slot />
+          </div>
+
+          <!-- FOOTER -->
+          <FooterContentPrivateLayout />
         </a-layout-content>
-        <a-layout-footer :style="{ textAlign: 'center' }">
-          Ant Design ©2018 Created by Ant UED
-        </a-layout-footer>
       </a-layout>
     </a-layout>
   </AntdConfig>
---
+++ shared/components/layout/private-layout/MenuSideMenuPrivateLayout.vue
@@ -0,0 +1,109 @@
+<script lang="ts" setup>
+const selectedKeys1 = ref<string[]>(['dashboard'])
+const openKeys = ref<string[]>(['sub1'])
+</script>
+
+<template>
+  <a-menu
+    v-model:selectedKeys="selectedKeys1"
+    v-model:openKeys="openKeys"
+    mode="inline"
+    :style="{ height: '100%', borderRight: 0 }"
+  >
+    <a-menu-item key="dashboard">
+      <nuxt-link href="/">
+        <user-outlined /> Dashboard
+      </nuxt-link>
+    </a-menu-item>
+    <a-menu-item key="basicQuery">
+      <nuxt-link href="/basic-query">
+        <user-outlined /> Basic Query
+      </nuxt-link>
+    </a-menu-item>
+    <a-menu-item key="about">
+      <nuxt-link href="/about">
+        <user-outlined /> About
+      </nuxt-link>
+    </a-menu-item>
+    <a-menu-item key="customAntd">
+      <nuxt-link href="/custom-antd">
+        <user-outlined /> Custom Antd
+      </nuxt-link>
+    </a-menu-item>
+    <a-sub-menu key="sub3">
+      <template #title>
+        <span>
+          <user-outlined />
+          Test
+        </span>
+      </template>
+      <a-menu-item key="1">
+        <nuxt-link href="/test/hasil">
+          Hasil
+        </nuxt-link>
+      </a-menu-item>
+    </a-sub-menu>
+    <a-sub-menu key="sub1">
+      <template #title>
+        <span>
+          <user-outlined />
+          subnav 1
+        </span>
+      </template>
+      <a-menu-item key="1">
+        option1
+      </a-menu-item>
+      <a-menu-item key="2">
+        option2
+      </a-menu-item>
+      <a-menu-item key="3">
+        option3
+      </a-menu-item>
+      <a-menu-item key="4">
+        option4
+      </a-menu-item>
+    </a-sub-menu>
+    <a-sub-menu key="sub2">
+      <template #title>
+        <span>
+          <laptop-outlined />
+          subnav 2
+        </span>
+      </template>
+      <a-menu-item key="5">
+        option5
+      </a-menu-item>
+      <a-menu-item key="6">
+        option6
+      </a-menu-item>
+      <a-menu-item key="7">
+        option7
+      </a-menu-item>
+      <a-menu-item key="8">
+        option8
+      </a-menu-item>
+    </a-sub-menu>
+    <a-sub-menu key="sub3">
+      <template #title>
+        <span>
+          <notification-outlined />
+          subnav 3
+        </span>
+      </template>
+      <a-menu-item key="9">
+        option9
+      </a-menu-item>
+      <a-menu-item key="10">
+        option10
+      </a-menu-item>
+      <a-menu-item key="11">
+        option11
+      </a-menu-item>
+      <a-menu-item key="12">
+        option12
+      </a-menu-item>
+    </a-sub-menu>
+  </a-menu>
+</template>
+
+<style lang="scss" scoped></style>
