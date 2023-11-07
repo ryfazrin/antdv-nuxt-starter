b77b37cdd4895f4995fce81693a324e135a6fbcc
ryfazrin <riansera515@gmail.com>
2023-11-07 15:23:15 +0700

    added: spin in app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
+<script lang="ts" setup>
+// const nuxtApp = useNuxtApp()
+const isLoading = ref(true)
+
+setTimeout(() => {
+  isLoading.value = false
+}, 2000)
+
+// nuxtApp.hook('page:start', () => {
+//   isLoading.value = true
+//   console.log('page:start')
+// })
+// nuxtApp.hook('page:finish', () => {
+//   isLoading.value = false
+//   console.log('page:finish')
+// })
+
+// onMounted(() => {
+//   isLoading.value = false
+//   console.log('onMounted')
+// })
+</script>
+
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <div v-if="isLoading" class="loading-spinner">
+      <a-spin size="large" />
+    </div>
+    <NuxtPage v-else />
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+.loading-spinner {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background: rgba(255, 255, 255, 0.7); */
+  background: rgba(255, 255, 255, 1);
+  z-index: 9999;
+}
+</style>
