75cc3bee5e31ed58b4b78ec9b9a0b9e7947bcdfb
ryfazrin <riansera515@gmail.com>
2023-11-08 10:32:17 +0700

    added: new components

---
+++ utils/debounce.js
@@ -0,0 +1,15 @@
+export const debounce = (func, time = 500) => {
+  let timer
+
+  return function (...args) {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const context = this
+
+    if (timer) clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      timer = null
+      func.apply(context, args)
+    }, time)
+  }
+}
